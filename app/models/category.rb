f44dd0d2d77439d40be679cf3da22cab5f77cf1f
sheylaPozo <shiggipoke@gmail.com>
2021-12-03 13:32:26 -0500

    fix dev

--- app/models/category.rb
+++
@@ -1,6 +0,0 @@
-class Category < ApplicationRecord
-  validates :name, :icon, presence: true, length: { maximum: 100 }
-
-  belongs_to :user
-  has_and_belongs_to_many :deals
-end
--- app/models/deal.rb
+++
@@ -1,7 +0,0 @@
-class Deal < ApplicationRecord
-  validates :name, :amount, presence: true, length: { maximum: 100 }
-  validates :amount, numericality: { greater_than: 0 }
-
-  belongs_to :author, class_name: 'User'
-  has_and_belongs_to_many :categories
-end
